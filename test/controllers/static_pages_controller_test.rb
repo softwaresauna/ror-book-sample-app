a4d46248b89805bb4707ff446a9c2fe833e30555
mbjelac <marko.bjelac@softwaresauna.com>
2018-05-15 09:33:09 +0200

    test-drove page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
+
+    assert_select 'title', expected_title('Home')
+
   end
 
-  test "should get help" do
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
+
+    assert_select 'title', expected_title('Help')
+
   end
 
-  test "should get about" do
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
+
+    assert_select 'title', expected_title('About')
+
+  end
+
+  def expected_title(prefix)
+    prefix + ' | Ruby on Rails Tutorial Sample App'
   end
 
 end
